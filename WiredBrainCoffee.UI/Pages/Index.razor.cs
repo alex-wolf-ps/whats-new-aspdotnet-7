03ce27d6a32673f1c5ebeeae1697620e3e6df92b
Alex Wolf <alexwolf@mcirosoft.com>
2022-11-18 09:07:02 -0500

    M03-end

--- WiredBrainCoffee.UI/Pages/Index.razor.cs
+++ WiredBrainCoffee.UI/Pages/Index.razor.cs
@@ -5,12 +5,13 @@ namespace WiredBrainCoffee.UI.Pages
     public partial class Index
     {
         [Inject]
-        public NavigationManager NavManager { get; set; }
-
+        NavigationManager NavManager { get; set; }
         public void ApplyPromo()
         {
-            NavManager.NavigateTo("order",
-                new NavigationOptions { HistoryEntryState = "WiredFall" });
+            NavManager.NavigateTo("/order", new NavigationOptions()
+            {
+                HistoryEntryState = "WiredDiscount"
+            });
         }
     }
 }
